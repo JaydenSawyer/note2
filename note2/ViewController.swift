10432a36eeeebb639931244bb9febd3e82993659
jayden Sawyer <jdnswr@gmail.com>
2024-09-23 21:15:08 -0700

    yo

--- note2/ViewController.swift
+++ note2/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func segment(_ sender: UISegmentedControl) {
+        var sel = sender.selectedSegmentIndex
+        switch sel{
+        case 0:
+            print("Blah")
+        case 1:
+            print("Jo")
+        default:
+            print("Didnt do anything")
+        }
+        
+    }
 }
-
